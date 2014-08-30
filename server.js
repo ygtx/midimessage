fe5e6dc093f0e2ca040268de33e4eb0564f2970a
yagithub <jagigyajujamobile@gmail.com>
2014-08-31 02:06:46 +0900

    server.jsに集めた

--- server.js
+++ server.js
@@ -4,25 +4,36 @@
 var http = require("http");
 var url = require("url");
 
+// var server = require("./server");
+var router = require("./route");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/select_sound"] = requestHandlers.selectSound;
+handle["/send_mail"] = requestHandlers.sendMailAndSave;
+handle["/load"] = requestHandlers.start;
+
+handle["/public"] = requestHandlers.publicDir;
+
+// function start(route, handle) {
+function onRequest(request, response) {
+
+    // PC and SP common
+
+    var pathname = url.parse(request.url).pathname;
+    if (pathname.lastIndexOf('/public',0) === 0) {
+        pathname = '/public';
+    } 
 
 
-function start(route, handle) {
-    function onRequest(request, response) {
-
-        // PC and SP common
-
-        var pathname = url.parse(request.url).pathname;
-        if (pathname.lastIndexOf('/public',0) === 0) {
-            pathname = '/public';
-        } 
-
-
-        route(handle, pathname, response, request);
-    }
-    var port = process.env.PORT || 1337;
-    http.createServer(onRequest).listen(port);
-    console.log("Server has started.");
+    router.route(handle, pathname, response, request);
 }
+var port = process.env.PORT || 1337;
+http.createServer(onRequest).listen(port);
+console.log("Server has started.");
+//}
 
-exports.start = start;
+// exports.start = start;
 
