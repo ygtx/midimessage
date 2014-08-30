ed537f92f07f889055f330d7e4f15f5b1143ec0a
yagithub <jagigyajujamobile@gmail.com>
2014-08-31 01:56:57 +0900

    デプロイする

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ function start(route, handle) {
 
         route(handle, pathname, response, request);
     }
-    http.createServer(onRequest).listen(8888);
+    var port = process.env.PORT || 1337;
+    http.createServer(onRequest).listen(port);
     console.log("Server has started.");
 }
 
